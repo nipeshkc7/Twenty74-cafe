c47e40e70888d2ea2acae8f892bd434dccd8a95f
nipeshkc7 <nipeshkc7@gmail.com>
2018-05-05 11:42:38 +1000

    contact form bug fixed

--- contactform/contactform.php
+++ contactform/contactform.php
@@ -3,7 +3,7 @@
 if($_POST){
     $name = $_POST['name'];
     $email = $_POST['email'];
-    $message = $_POST['message`'];
+    $message = $_POST['message'];
 
 //send email
     mail("janak@twenty74.com.au", "Feedback from" .$email, $message);
